fb34a70edaada3a3d20334ce27439da92f2d7637
Aryan Bajad <aryanbajad2@gmail.com>
2025-03-14 15:51:31 +0000

    conversionOperation

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,11 @@
+let score = null
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
