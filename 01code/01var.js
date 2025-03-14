e02b735667f3dbf7f7c91ca0ff933bd178d3e8b1
Aryan Bajad <aryanbajad2@gmail.com>
2025-03-14 10:38:48 +0000

    variables and constants

---
+++ 01code/01var.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail ="aryan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kalyan"
+
+accountEmail = "ab@gmail.com"
+accountPassword = "123234435"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
