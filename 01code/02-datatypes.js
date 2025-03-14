3e71153c736c7cde8f968dd4ee2095ca37004f00
Aryan Bajad <aryanbajad2@gmail.com>
2025-03-14 15:13:42 +0000

    datatypes

---
+++ 01code/02-datatypes.js
@@ -0,0 +1,12 @@
+let name ="Aryan"
+let age = 18
+let isLoggedIn = false
+let temperature = null
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// noolean => true/false
+// null => standalone value (null is an object type)
+// undefined => 
+// symbol => unique
