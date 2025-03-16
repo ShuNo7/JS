9ef30d53452f64a5f12c3997269d74d3aa58054d
Aryan Bajad <aryanbajad2@gmail.com>
2025-03-16 09:42:14 +0000

    number math

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -1,11 +1,28 @@
 let score = null
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
---
+++ 01-basics/04-string.js
@@ -0,0 +1,12 @@
+const name = "Aryan"
+const repoCount = 50
+
+console.log(`Hello this is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+const gameName = new String('Aryan')
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('n'));
+
