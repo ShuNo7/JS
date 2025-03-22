c48ce6abb63246dda1b03f890ea3cd79061f873f
Aryan Bajad <aryanbajad2@gmail.com>
2025-03-22 07:26:50 +0000

    iteration

---
+++ 05-iteration/iteration.js
@@ -0,0 +1,17 @@
+// for (let i = 1; i <=10; i++){
+//     //   console.log(`Outer loop value: ${i}`)
+//     for(let j = 1; j <=10; j++){
+//         // console.log(`Inner loop value ${j} and inner loop ${i}`)
+//         // console.log(i + '*' + j + '=' + i*j)
+//     }
+// }
+
+// do while loop
+ 
+
+let score = 10
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
